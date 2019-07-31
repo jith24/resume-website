d6ce74ace007ab3e35cbe2f0d106793f8830ad2f
Abhijith Nandagopal <abhijith.nandagopal24@gmail.com>
2019-07-31 20:16:45 +0530

    Updates

---
+++ precache-manifest.4734994073626d00a25204c3e10095d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b20de63da9391dd65e6b938ecd71b722",
+    "url": "./static/media/profilepic.b20de63d.JPG"
+  },
+  {
+    "revision": "76b999b2deab65d4827f40c2d5cf2d69",
+    "url": "./static/media/AbhijithNandagopal_Resume.76b999b2.pdf"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "571b2247295c58b15aed",
+    "url": "./static/js/main.571b2247.chunk.js"
+  },
+  {
+    "revision": "af11ee862768508da08c",
+    "url": "./static/js/1.af11ee86.chunk.js"
+  },
+  {
+    "revision": "571b2247295c58b15aed",
+    "url": "./static/css/main.15eba0bd.chunk.css"
+  },
+  {
+    "revision": "b02147fbfe01324d8b28b1902e3d50a6",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/resume-website/precache-manifest.c130841591d137aec81fccbda2add818.js"
+  "./precache-manifest.4734994073626d00a25204c3e10095d6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume-website/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
